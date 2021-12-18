4d908d9bb8ad90a9639af14f7f2a5da8b7a7304e
Maxim Kasyanenko <maxim.kasyanenko@protonmail.ch>
2021-12-18 13:48:52 -0800

    Use image struct

--- ascii.cpp
+++ ascii.cpp
@@ -1,78 +1,73 @@
-#include <iostream>
+#include <cstdio>
 #include <string>
-#include <tuple>
+
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 #define STB_IMAGE_RESIZE_IMPLEMENTATION
 #include "stb_image_resize.h"
 
 
-class LoadImage {
-public:
-    int x, y, n;
-    unsigned char * data;
-    LoadImage (char* filename) {
-        int ox, oy;
-		unsigned char * odata = stbi_load(filename, &ox, &oy, &n, 0);
-        data = stbi_load(filename, &x, &y, &n, 0);
-        x = 100; 
-		y = (int)(x * oy / (2 * (float)ox));
-
-        data = new unsigned char[x * y * n];
-        stbir_resize_uint8(odata, ox, oy, 0, data, x, y, 0, n);
-        stbi_image_free(odata);
-    }
-	~LoadImage() { delete[] data; }
-
-    std::tuple<unsigned char, unsigned char, unsigned char> getPixel(int x, int y) {
-        unsigned char r = data[(y * this->x + x) * n];
-        unsigned char g = data[(y * this->x + x) * n + 1];
-        unsigned char b = data[(y * this->x + x) * n + 2];
-        return {r, g, b};
-    }
+struct RGBA {
+	int r, g, b, a;
 };
 
-int distanceCalculator(std::tuple<unsigned char, unsigned char, unsigned char> a, std::tuple<unsigned char, unsigned char, unsigned char> b) {
-    int x = std::get<0>(a) - std::get<0>(b);
-    int y = std::get<1>(a) - std::get<1>(b);
-    int z = std::get<2>(a) - std::get<2>(b);
+struct Image {
+	unsigned char *buf;
+	int w, h, n;
 
-    return x*x + y*y + z*z;
-}
+	Image(std::string const &fp) {
+		buf = stbi_load(fp.c_str(), &w, &h, &n, 0);
+	}
 
-int choose_color(std::tuple<unsigned char, unsigned char, unsigned char> rgb) {
-    int minValue = INT_MAX;
-    int minIndex = 0;
-    for (int i = 0; i < 256; i++) {
-        int currDistance = distanceCalculator(rgb, colors[i]);
-        if (currDistance < minValue) {
-            minValue = currDistance;
-            minIndex = i;
-        }
-    }
+	~Image() {
+		stbi_image_free(buf);
+	}
 
-    return minIndex;
-}
+	void getPixel(RGBA &px, int x, int y) {
+		switch (n) {
+		case 1: {
+			int e = buf[y+w+x];
+			px.r = e;
+			px.g = e;
+			px.b = e;
+			px.a = 255;
+			break;
+		}
+		case 2: {
+			px.r = buf[y*w+x];
+			px.g = buf[y*w+x+1];
+			px.b = 0;
+			px.a = 255;
+			break;
+		}
+		case 3: {
+			px.r = buf[y*w+x];
+			px.g = buf[y*w+x+1];
+			px.b = buf[y*w+x+2];
+			px.a = 255;
+			break;
+		}
+		case 4: {
+			px.r = buf[y*w+x];
+			px.g = buf[y*w+x+1];
+			px.b = buf[y*w+x+2];
+			px.a = buf[y*w+x+3];
+			break;
+		}
+		}
+	}
+};
+
 
 int main (int argc, char** argv) {
-    std::string codelib = "B8&WM#YXQO{}[]()I1i!pao;:,.    ";
-    int count = codelib.size();
-    LoadImage currImage = LoadImage(argv[1]);
+    Image image {argv[1]};
 
-    std::cout << "\033[0;0H";
-    for (int y = 0; y < currImage.y; y++) {
-        for (int x = 0;  x < currImage.x; x++) {
-            auto rgb = currImage.getPixel(x, y);
-            int r = std::get<0>(rgb);
-            int g = std::get<1>(rgb);
-            int b = std::get<2>(rgb);
-
-            float gray = 0.299 * r + 0.587 * g + 0.114 * b;
-            int currIndex = choose_color(rgb);
-
-			std::printf("\033[48;2;%d;%d;%dm ", r, g, b);
-            // std::cout << "\033[38;5;" + std::to_string(currIndex) + 'm' + codelib[(int)((count - 1) * gray / 255.0f)];
-            // std::cout << codelib[(int)((count - 1) * gray / 255.0f)];
+	std::puts("\033[1;1H");
+    for (int y = 0; y < image.h; y++) {
+        for (int x = 0;  x < image.w; x++) {
+			RGBA px;
+			image.getPixel(px, x, y);
+			std::printf("\033[48;2;%d;%d;%dm ", px.r, px.g, px.b);
         }
 		std::puts("");
     }
