49bc9cb11dc357372ea6a816be81dc733bccd51c
Maxim Kasyanenko <maxim.kasyanenko@protonmail.ch>
2021-12-18 15:12:41 -0800

    Use ioctl for W/H, add getPixelV

--- ascii.cpp
+++ ascii.cpp
@@ -1,10 +1,9 @@
-#include <iostream>
 #include <cstdio>
 #include <string>
 
 extern "C" {
-#include <termios.h>
 #include <sys/ioctl.h>
+#include <unistd.h>
 }
 
 #define STB_IMAGE_IMPLEMENTATION
@@ -17,12 +16,18 @@ struct RGBA {
 	int r, g, b, a;
 };
 
+struct View {
+	int w, h;
+};
+
 struct Image {
+	static constexpr int channels {4};
+
 	unsigned char *buf;
 	int w, h, n;
 
 	Image(std::string const &fp) {
-		buf = stbi_load(fp.c_str(), &w, &h, &n, 0);
+		buf = stbi_load(fp.c_str(), &w, &h, &n, channels);
 	}
 
 	~Image() {
@@ -30,37 +35,18 @@ struct Image {
 	}
 
 	void getPixel(RGBA &px, int x, int y) {
-		switch (n) {
-		case 1: {
-			int e = buf[y+w+x];
-			px.r = e;
-			px.g = e;
-			px.b = e;
-			px.a = 255;
-			break;
-		}
-		case 2: {
-			px.r = buf[y*w+x];
-			px.g = buf[y*w+x+1];
-			px.b = 0;
-			px.a = 255;
-			break;
-		}
-		case 3: {
-			px.r = buf[y*w+x];
-			px.g = buf[y*w+x+1];
-			px.b = buf[y*w+x+2];
-			px.a = 255;
-			break;
-		}
-		case 4: {
-			px.r = buf[y*w+x];
-			px.g = buf[y*w+x+1];
-			px.b = buf[y*w+x+2];
-			px.a = buf[y*w+x+3];
-			break;
-		}
-		}
+		std::size_t off = 4*(y*w+x);
+		px.r = buf[off];
+		px.g = buf[off+1];
+		px.b = buf[off+2];
+		px.a = buf[off+3];
+	}
+
+	void getPixelV(RGBA &px, View const &view, int x, int y) {
+		/* nearest neighbor for now */
+		x = (int)(x * (float) w / view.w);
+		y = (int)(y * (float) h / view.h);
+		getPixel(px, x, y);
 	}
 };
 
@@ -68,29 +54,21 @@ struct Image {
 int 
 main(int argc, char* argv[]) 
 {
-	/* configure stdin so we can query terminal info */
-	struct termios settings;
-	tcgetattr(0, &settings);
-	settings.c_lflag &= ~(ECHO | ICANON);
-	tcsetattr(0, TCSAFLUSH, &settings);
+	/* get terminal dimensions (posix) */
+	struct winsize ws;
+	ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws);
 
-	/* */
-	std::printf("\033[18;12H");
-	std::printf("\033[6n");
-	char a, b, c;
-	std::cin >> c >> b >> c;
-	std::cout << a << b << c << std::endl;
-//	std::getline(std::cin, line);
-//	std::cout << line << std::endl;
-//	Image image {argv[1]};
-//	
-//	std::puts("\033[1;1H");
-//	for (int y = 0; y < image.h; y++) {
-//		for (int x = 0;  x < image.w; x++) {
-//			RGBA px;
-//			image.getPixel(px, x, y);
-//			std::printf("\033[48;2;%d;%d;%dm ", px.r, px.g, px.b);
-//		}
-//		std::puts("");
-//	}
+	/* load and write image */
+	Image im {argv[1]};
+	View view { ws.ws_col, ws.ws_row };
+
+	std::printf("\033[1;1H");
+	for (int y = 0; y < ws.ws_row; ++y) {
+		for (int x = 0; x < ws.ws_col; ++x) {
+			RGBA px;
+			im.getPixelV(px, view, x, y);
+			std::printf("\033[48;2;%d;%d;%dm ", px.r, px.g, px.b);
+		}
+		std::puts("");
+	}
 }
