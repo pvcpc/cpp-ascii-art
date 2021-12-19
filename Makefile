69414ea9dc810ae112d368910b15bff3a321039f
Maxim Kasyanenko <maxim.kasyanenko@protonmail.ch>
2021-12-18 17:06:29 -0800

    Use OpenCV for images instead

--- Makefile
+++ Makefile
@@ -1,9 +1,9 @@
 CC := /usr/bin/g++
+CFLAGS := -O2 -mtune=native -march=native
+CFLAGS += $(shell pkg-config --cflags --libs opencv4)
 
 ifdef DEBUG
-	CFLAGS := -Og -g
-else
-	CFLAGS := -O2 -mtune=native -march=native
+	CFLAGS += -Og -g
 endif
 
 srcs := $(wildcard *.cpp)
--- ascii.cpp
+++ ascii.cpp
@@ -1,72 +1,77 @@
 #include <cstdio>
 #include <string>
 
-extern "C" {
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/videoio.hpp>
+
+extern "C" 
+{
 #include <sys/ioctl.h>
 #include <unistd.h>
 }
 
-#define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.h"
-#define STB_IMAGE_RESIZE_IMPLEMENTATION
-#include "stb_image_resize.h"
 
-
-struct RGBA {
-	int r, g, b, a;
+struct RGB 
+{
+	int r, g, b;
 };
 
-struct View {
+/* swizzling: how RGB data is extracted, ex. BGR -> RGB */
+using Swiz = RGB;
+static constexpr Swiz
+	SWIZ_RGB = {0, 1, 2},
+	SWIZ_BGR = {2, 1, 0};
+
+struct View
+{
 	int w, h;
 };
 
-struct Image {
-	static constexpr int channels {4};
+static void 
+getPixel(
+	RGB &px, 
+	cv::Mat const &im,
+	int x, 
+	int y,
+	Swiz const &sw = SWIZ_BGR
+) {
+	std::size_t off = im.channels() * (y * im.cols + x);
+	px.r = im.data[off+sw.r];
+	px.g = im.data[off+sw.g];
+	px.b = im.data[off+sw.b];
+}
 
-	unsigned char *buf;
-	int w, h, n;
-
-	Image(std::string const &fp) {
-		buf = stbi_load(fp.c_str(), &w, &h, &n, channels);
-	}
-
-	~Image() {
-		stbi_image_free(buf);
-	}
-
-	void getPixel(RGBA &px, int x, int y) {
-		std::size_t off = 4*(y*w+x);
-		px.r = buf[off];
-		px.g = buf[off+1];
-		px.b = buf[off+2];
-		px.a = buf[off+3];
-	}
-
-	void getPixelV(RGBA &px, View const &view, int x, int y) {
-		/* nearest neighbor for now */
-		x = (int)(x * (float) w / view.w);
-		y = (int)(y * (float) h / view.h);
-		getPixel(px, x, y);
-	}
-};
+static void 
+getPixelV(
+	RGB &px, 
+	cv::Mat const &im,
+	View const &view, 
+	int x, 
+	int y, 
+	Swiz const &sw = SWIZ_BGR
+) {
+	/* nearest neighbor for now */
+	x = (int)(x * (float) im.cols / view.w);
+	y = (int)(y * (float) im.rows / view.h);
+	getPixel(px, im, x, y, sw);
+}
 
 
 int 
 main(int argc, char* argv[]) 
 {
-	/* get terminal dimensions (posix) */
+	/* */
 	struct winsize ws;
 	ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws);
 
-	/* load and write image */
-	Image im {argv[1]};
-	View view { ws.ws_col, ws.ws_row };
-
-	std::printf("\033[1;1H");
+	/* */
+	cv::Mat im = cv::imread(argv[1]);
+	View view {ws.ws_col, ws.ws_row};
+	std::printf("\033[0;0H");
 	for (int y = 0; y < ws.ws_row; ++y) {
 		for (int x = 0; x < ws.ws_col; ++x) {
-			RGBA px;
-			im.getPixelV(px, view, x, y);
+			RGB px;
+			getPixelV(px, im, view, x, y);
 			std::printf("\033[48;2;%d;%d;%dm ", px.r, px.g, px.b);
 		}
 		std::puts("");
